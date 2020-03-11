b57588198f26b59f36f95d772dbfabd2b3cdae4c
Mantas Eringis <mantaseringis@gmail.com>
2020-03-11 22:16:11 +0200

    remove task description

--- 01_NthFib/nThFib.go
+++ 01_NthFib/nThFib.go
@@ -4,16 +4,6 @@ import (
 	"fmt"
 )
 
-// The Fibonacci sequence is defined as follows: the first number of the sequence is 0, the second number is 1,
-// and the nth number is the sum of the (n - 1)th and (n - 2)th numbers. Write a function that takes in an integer n
-// and returns the nth Fibonacci number.
-
-// Important note: the Fibonacci sequence is often defined with its first 2 numbers as F0 = 0 and F1 = 1.
-// For the purpose of this question, the first Fibonacci number is F0; therefore, getNthFib(1) is equal to F0, getNthFib(2) is equal to F1, etc..
-
-// Sample input #2: 6
-// Sample output #2: 5 (0, 1, 1, 2, 3, 5)
-
 func main() {
 	getNthFib(10)
 }
--- 02_TwoNumberSum/twoNumberSum.go
+++ 02_TwoNumberSum/twoNumberSum.go
@@ -4,14 +4,6 @@ import (
 	"fmt"
 )
 
-// Write a function that takes in a non-empty array of distinct integers and an integer representing a target sum.
-// If any two numbers in the input array sum up to the target sum, the function should return them in an array.
-// If no two numbers sum up to the target sum, the function should return an empty array.
-// Assume that there will be at most one pair of numbers summing up to the target sum.
-
-// Sample input: [3, 5, -4, 8, 11, 1, -1, 6], 10
-// Sample output: [-1, 11]
-
 func main() {
 	inputArray := []int{3, 5, -4, 8, 11, 1, -1, 6}
 	targetSum := 10
